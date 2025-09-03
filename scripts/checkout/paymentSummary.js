2f157a2e64a9824ad100a35931492bb6a6431553
Nnaemeka <codebasetwo@gmail.com>
2025-09-03 20:40:08 +0100

    add url parameters and tracking page

--- scripts/checkout/paymentSummary.js
+++ scripts/checkout/paymentSummary.js
@@ -2,6 +2,7 @@ import {cart} from '../../data/cart.js'
 import { getDeliveryOption } from '../../data/deliveryOptions.js';
 import { getProduct } from '../../data/products.js';
 import formatCurrency from '../utils/money.js';
+import { addOrder } from '../../data/order.js';
 
 export function renderPayment(){
     let productPriceCents = 0;
@@ -49,10 +50,35 @@ export function renderPayment(){
             <div class="payment-summary-money">$${formatCurrency(totalCents)}</div>
           </div>
 
-          <button class="place-order-button button-primary">
+          <button class="place-order-button button-primary js-place-order">
             Place your order
           </button>`
 
     document.querySelector('.js-payment-summary')
         .innerHTML = paymentSummaryHTML;
+
+    document.querySelector('.js-place-order')
+    .addEventListener('click', async () => {
+      try {
+        const response = await fetch('https://supersimplebackend.dev/orders', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(
+          {
+            cart: cart
+
+          }
+        )
+      });
+      const order = await response.json();
+      console.log(order);
+      addOrder(order);
+
+      } catch (error){
+        console.log('unexpected error try again later.')
+      }
+      window.location.href = 'orders.html'
+})
 }
